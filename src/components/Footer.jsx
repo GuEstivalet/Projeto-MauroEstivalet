cb2cae258ea5a2e74d1c2c2456eefd8d65d0b3d4
Gustavo Ulysséa Estivalet <131898301+GuEstivalet@users.noreply.github.com>
2024-10-29 21:36:30 -0300

    Ajustes no Header e Footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import styles from './Footer.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'; // Importa o ícone do WhatsApp
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'; // Importa o ícone do Instagram
+import { faPix } from '@fortawesome/free-brands-svg-icons';
+import { faCreditCard } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className={styles.footer}>
+      <h1>Conheça nossas redes sociais</h1>
+      <a href=''><FontAwesomeIcon className={styles.icon} icon={faWhatsapp} /></a>
+      <a href=''><FontAwesomeIcon className={styles.icon} icon={faInstagram} /></a>
+      <h2>Aceitamos dinheiro, pix e cartões de crédito</h2>
+      <FontAwesomeIcon className={styles.icon} icon={faCreditCard} />
+      <FontAwesomeIcon className={styles.icon} icon={faPix} />
+    </section>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
    <nav className={styles.header}>
       <ul>
-        <li><NavLink className={styles.logo} to='/' end>Logo</NavLink></li>
+        <li><NavLink className={styles.logo} to='/' end><img src="./src/assets/logo-mauro.jpg" alt="Logo do consultório odontológico Mauro Estivalet" /></NavLink></li>
         <li><NavLink className={styles.link} to='/' end>Início</NavLink></li>
         <li><NavLink className={styles.link} to='projeto'>Projeto</NavLink></li>
         <li><NavLink className={styles.link} to='contato'>Contato</NavLink></li>
