bbc713976b199017401e061c6388e85a0ae83305
Gustavo Ulysséa Estivalet <131898301+GuEstivalet@users.noreply.github.com>
2024-10-31 11:50:51 -0300

    Tentativa de correção em Header e Footer.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ import { NavLink } from 'react-router-dom'
 const Header = () => {
   return (
    <nav className={styles.header}>
+      <NavLink className={styles.logo} to='/' end><img src="./src/assets/logo-mauro.jpg" alt="Logo do consultório odontológico Mauro Estivalet" /></NavLink>
       <ul>
-        <li><NavLink className={styles.logo} to='/' end><img src="./src/assets/logo-mauro.jpg" alt="Logo do consultório odontológico Mauro Estivalet" /></NavLink></li>
         <li><NavLink className={styles.link} to='/' end>Início</NavLink></li>
         <li><NavLink className={styles.link} to='projeto'>Projeto</NavLink></li>
         <li><NavLink className={styles.link} to='contato'>Contato</NavLink></li>
--- src/components/Tratamentos.jsx
+++ src/components/Tratamentos.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import styles from "./Tratamentos.module.css"
 
 const Tratamentos = () => {
   return (
-    <div>
-      
+    <div className={styles.elementoPai}>
+      <div className={styles.wrapper}>
+        <div className={styles.card}>
+          <h1>Lorem Ipsum</h1>
+          <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</p>
+        </div>
+        <div className={styles.card}>
+        <h1>Lorem Ipsum</h1>
+          <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</p>
+        </div>
+        <div className={styles.card}>
+        <h1>Lorem Ipsum</h1>
+          <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</p>
+        </div>
+        <div className={styles.card}>
+        <h1>Lorem Ipsum</h1>
+          <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</p>
+        </div>
+      </div>
+      <div className={styles.wrapper}>
+        <div className={styles.card}>
+        <h1>Lorem Ipsum</h1>
+          <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</p>
+        </div>
+        <div className={styles.card}>
+        <h1>Lorem Ipsum</h1>
+          <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</p>
+        </div>
+        <div className={styles.card}>
+        <h1>Lorem Ipsum</h1>
+          <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</p>
+        </div>
+        <div className={styles.card}>
+        <h1>Lorem Ipsum</h1>
+          <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</p>
+        </div>
+      </div>
     </div>
   )
 }
