818a3b05249dc609bce447dd2f5222ecdd36c71a
Gustavo Ulysséa Estivalet <131898301+GuEstivalet@users.noreply.github.com>
2024-11-01 12:11:22 -0300

    Modificações no Header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
-import styles from './Header.module.css'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import styles from './Header.module.css';
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
+
+  const [buttonMenu, setButtonMenu] = React.useState(false);
+
+  function onClick () {
+    setButtonMenu(!buttonMenu);
+  }
+
   return (
-   <nav className={styles.header}>
+   <header>
+      <nav className={styles.navBar}>
       <NavLink className={styles.logo} to='/' end><img src="./src/assets/logo-mauro.jpg" alt="Logo do consultório odontológico Mauro Estivalet" /></NavLink>
       <ul>
         <li><NavLink className={styles.link} to='/' end>Início</NavLink></li>
         <li><NavLink className={styles.link} to='projeto'>Projeto</NavLink></li>
         <li><NavLink className={styles.link} to='contato'>Contato</NavLink></li>
       </ul>
-   </nav>
+      </nav>
+
+    <nav className={buttonMenu ? `${styles.mobileMenuOn}` : `${styles.mobileMenu}`}>
+    <NavLink className={styles.logo} to='/' end><img src="./src/assets/logo-mauro.jpg" alt="Logo do consultório odontológico Mauro Estivalet" /></NavLink>
+      <ul className={buttonMenu ? `${styles.buttonOnLista}` : `${styles.buttonOffLista}`}>
+        <li><NavLink className={styles.mobileLinks} to='/' end>Início</NavLink></li>
+        <li><NavLink className={styles.mobileLinks} to='projeto'>Projeto</NavLink></li>
+        <li><NavLink className={styles.mobileLinks} to='contato'>Contato</NavLink></li>
+      </ul>
+     <button onClick={onClick} className={buttonMenu ? `${styles.buttonOnBtn}` : `${styles.buttonOffBtn}`}><FontAwesomeIcon icon={faBars} /></button>
+     
+    </nav>
+   </header>
   )
 }
 
